fcb2279a964c0dd05a0621495eecb7db5e435f0f
rgayan12 <rgayan@rocketmail.com>
2021-12-09 22:14:00 +0000

    Created and updated layouts, routes and javascript files

---
+++ resources/views/layout/base.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    @yield('pageTitle')
+
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.13.0/css/all.css"
+            integrity="sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V"
+            crossorigin="anonymous"
+    />
+    @yield('stylesheets')
+</head>
+
+<body>
+@include('layout.navigation')
+
+@yield('content')
+
+<script src="{{asset('js/scripts.js')}}"></script>
+@yield('scripts')
+</body>
+</html>
